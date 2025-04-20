e9c4c302b9561943e010966f120d27d71e4131aa
pooja <ghadgepooja1812@gmail.com>
2025-04-20 15:22:35 +0530

    subtracting numbers

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Addition{
+    public static void main(String args[])
+    {
+        int a=30;
+        int b=20;
+        int diff=a-b;
+        System.out.println("This is program for subtracting two numbers");
+        System.out.println("Sum of a and b is "+diff)
+    }
+}
