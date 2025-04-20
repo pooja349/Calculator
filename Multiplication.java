f38483765ce69f5fdfe39bacd40a50c5145b604b
pooja <ghadgepooja1812@gmail.com>
2025-04-20 15:26:35 +0530

    multiplying numbers

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+public class Addition{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int result=a*b;
+        System.out.println("This is program for muliplying two numbers");
+        System.out.println("Multiplication of a and b is "+result)
+    }
+}
